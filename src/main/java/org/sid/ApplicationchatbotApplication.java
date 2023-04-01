86ca7bd7976cc58b6db02583055b8efc0862911f
Wassim Abidi <wasimabidi01@gmail.com>
2023-04-01 23:48:42 +0200

    ApplicationChat

---
+++ src/main/java/org/sid/ApplicationchatbotApplication.java
@@ -0,0 +1,14 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class ApplicationchatbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationchatbotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/sid/Controller/ChatController.java
@@ -0,0 +1,25 @@
+package org.sid.Controller;
+
+import org.sid.models.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+@MessageMapping("/chat.registrer")
+@SendTo("/topic/public")
+public ChatMessage register (@Payload ChatMessage chatMessage,SimpMessageHeaderAccessor headerAccessor)
+{
+	headerAccessor.getSessionAttributes().put("username",chatMessage.getSender());
+	return chatMessage;
+}
+@MessageMapping("/chat.send")
+@SendTo("/topic/public")
+public ChatMessage sendMessage(@Payload ChatMessage chatMessage)
+{
+ return chatMessage;
+}
+}
